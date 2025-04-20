74229de72c47a78b8621b6fb3f0a03f97ec1d20c
Nicole Bond <nicbond22@gmail.com>
2025-04-20 15:13:00 -0400

    Commented out initial lesson code and variables. Began new code for lesson practice

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,25 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+//    Initial lesson info
+//    @State private var name = ""
+//    @State private var textTitle = "What is your name?"
     
-    @State private var name = ""
-    @State private var textTitle = "What is your name?"
+//    Lesson Practice Info
+    @State private var newName = ""
     
     var body: some View {
+//    Initial lesson code
+        /*        VStack {
+         
+         Text(textTitle)
+         .font(.title)
+         TextField("Type your name here...", text: $name)
+         .multilineTextAlignment(.center)
+         .font(.title)
+         .border(Color.gray, width: 2)
+         Button("Submit Name") {
+         textTitle = "Welcome, \(name)!"
+         }
+         .font(.title2)
+         .buttonStyle(.borderedProminent)
+         .tint(.purple)
+         }
+         .padding()
+         */
+        
+//    Lesson Practice Code
+        
+        
         VStack {
-            
-            Text(textTitle)
-                .font(.title)
-            TextField("Type your name here...", text: $name)
+            Text("Please answer the following questions and submit your responses when finished")
+                .font(.largeTitle)
+                .fontWeight(.bold)
                 .multilineTextAlignment(.center)
-                .font(.title)
-                .border(Color.gray, width: 2)
-            Button("Submit Name") {
-                textTitle = "Welcome, \(name)!"
-            }
-            .font(.title2)
-            .buttonStyle(.borderedProminent)
-            .tint(.purple)
+        }
+        .padding()
+        
+        VStack {
+            Text("What is your name?")
+            TextField("Type your name here...", text: $newName)
         }
         .padding()
     }

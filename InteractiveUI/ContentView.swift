a90c88cfdfe6906e041160451d253d23d1244dce
Nicole Bond <nicbond22@gmail.com>
2025-04-20 15:42:51 -0400

    Added in 2 user input fields and submissions print a statement into the console when submit button is pressed.  Other style modifications were added.  Included modification to favorite color text field so that words aren't automatically capitalized

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -14,43 +14,89 @@ struct ContentView: View {
     
 //    Lesson Practice Info
     @State private var newName = ""
+    @State private var favColor = ""
     
     var body: some View {
-//    Initial lesson code
-        /*        VStack {
-         
-         Text(textTitle)
-         .font(.title)
-         TextField("Type your name here...", text: $name)
-         .multilineTextAlignment(.center)
-         .font(.title)
-         .border(Color.gray, width: 2)
-         Button("Submit Name") {
-         textTitle = "Welcome, \(name)!"
-         }
-         .font(.title2)
-         .buttonStyle(.borderedProminent)
-         .tint(.purple)
-         }
-         .padding()
-         */
-        
-//    Lesson Practice Code
-        
-        
-        VStack {
-            Text("Please answer the following questions and submit your responses when finished")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
+        ZStack {
+            Color(.systemGray5)
+                .ignoresSafeArea()
+            
+            //    Initial lesson code
+            /*        VStack {
+             
+             Text(textTitle)
+             .font(.title)
+             TextField("Type your name here...", text: $name)
+             .multilineTextAlignment(.center)
+             .font(.title)
+             .border(Color.gray, width: 2)
+             Button("Submit Name") {
+             textTitle = "Welcome, \(name)!"
+             }
+             .font(.title2)
+             .buttonStyle(.borderedProminent)
+             .tint(.purple)
+             }
+             .padding()
+             */
+            
+            //    Lesson Practice Code
+            
+            // General app instructions
+            VStack (spacing: 50) {
+                
+                VStack {
+                    Text("Please answer the following questions and submit your responses when finished!")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.purple)
+                }
+                .padding()
+                
+                // Name question and text field
+                VStack {
+                    Text("What is your name?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    TextField("Type your name here...", text: $newName)
+                        .multilineTextAlignment(.center)
+                        .border(Color.green, width: 3)
+                        .background(.white)
+                        .font(.title2)
+                }
+                .padding()
+                
+                
+                // Favorite color question and text field
+                VStack {
+                    Text("What is your favorite color?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    TextField("Type the color here...", text: $favColor)
+                        .multilineTextAlignment(.center)
+                        .border(Color.green, width: 3)
+                        .background(.white)
+                        .font(.title2)
+//  researched code to stop text field from auto capitalizing the word entered
+                        .textInputAutocapitalization(.never)
+                }
+                .padding()
+                
+                VStack {
+                    Button("Submit Responses") {
+                        print("Your name is \(newName) and your favorite color is \(favColor)! 🤗")
+                    }
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.purple)
+                }
+                .padding()
+            }
         }
-        .padding()
-        
-        VStack {
-            Text("What is your name?")
-            TextField("Type your name here...", text: $newName)
-        }
-        .padding()
     }
 }
 
